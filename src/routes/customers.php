38413dcefdd285062316d69daa32a67efbc022b5
Paweł Tryfon <pawelxtryfon@gmail.com>
2018-05-13 10:37:28 +0200

    Initializing commit with basic php routing logic.

---
+++ src/routes/customers.php
@@ -0,0 +1,12 @@
+<?php
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+$app = new \Slim\App;
+
+// Get all Customers
+
+$app->get('/api/customers', function (Request $request, Response $response) {
+    echo 'CUSTOMERS';
+});
